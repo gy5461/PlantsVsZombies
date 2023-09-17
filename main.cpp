9be86da3f2f8c37e0c63de62a61035c3ad2608d9
gy5461 <1900269631@qq.com>
2023-09-17 18:09:06 +0800

    拖动植物

--- main.cpp
+++ main.cpp
@@ -25,6 +25,22 @@ enum
 IMAGE imgBg;	//表示背景图片
 IMAGE imgBar;	//表示工具栏图片
 IMAGE imgCards[PLANT_COUNT];
+IMAGE *imgPlants[PLANT_COUNT][20];
+
+int curX, curY;	//当前选中的植物，在移动过程中的位置
+int curPlant; // 0: 没有选中，1: 选中第一种
+
+bool fileExist(const char* name)
+{
+	FILE* fp = fopen(name, "r");
+	if (fp == NULL)
+	{
+		return false;
+	}
+
+	fclose(fp);
+	return true;
+}
 
 void gameInit()
 {
@@ -32,6 +48,8 @@ void gameInit()
 	// 把字符集改成多字节字符集
 	loadimage(&imgBg, "res/bg.jpg");
 	loadimage(&imgBar, "res/bar5.png");
+
+	memset(imgPlants, 0, sizeof(imgPlants));
 	
 	//初始化植物卡牌
 	char name[64];
@@ -40,14 +58,32 @@ void gameInit()
 		// 生成植物卡牌的文件名
 		sprintf_s(name, sizeof(name), "res/Cards/card_%d.png", i + 1);
 		loadimage(&imgCards[i], name);
+
+		for (int j = 0; j < 20; j++)
+		{
+			sprintf_s(name, sizeof(name), "res/zhiwu/%d/%d.png", i, j + 1);
+			// 先判断这个文件是否存在
+			if (fileExist(name))
+			{
+				imgPlants[i][j] = new IMAGE;
+				loadimage(imgPlants[i][j], name);
+			}
+			else
+			{
+				break;
+			}
+		}
 	}
 
+	curPlant = 0;
 	// 创建游戏的图形窗口
-	initgraph(WIN_WIDTH, WIN_HEIGHT);
+	initgraph(WIN_WIDTH, WIN_HEIGHT, 1);
 }
 
 void updateWindow()
 {
+	BeginBatchDraw();	//开始缓冲
+
 	putimage(0, 0, &imgBg);
 	putimagePNG(250, 0, &imgBar);
 
@@ -57,13 +93,55 @@ void updateWindow()
 		int y = 6;
 		putimage(x, y, &imgCards[i]);
 	}
+
+	// 渲染拖动过程中的植物
+	if (curPlant > 0)
+	{
+		IMAGE* img = imgPlants[curPlant - 1][0];
+		putimagePNG(curX - img->getwidth() / 2, curY - img->getheight() / 2, img);
+	}
+
+	EndBatchDraw();	//结束双缓冲
+}
+
+void userClick()
+{
+	ExMessage msg;
+	static int status = 0;
+	if (peekmessage(&msg))
+	{
+		if (msg.message == WM_LBUTTONDOWN)
+		{
+			if (msg.x > 338 && msg.x < 338 + 65 * PLANT_COUNT && msg.y >6 && msg.y < 96)
+			{
+				int index = (msg.x - 338) / 65;
+				status = 1;
+				curPlant = index + 1;
+			}
+		}
+		else if (msg.message == WM_MOUSEMOVE && status == 1)
+		{
+			curX = msg.x;
+			curY = msg.y;
+		}
+		else if (msg.message == WM_LBUTTONUP)
+		{
+
+		}
+	}
+
 }
 
 int main(void)
 {
 	gameInit();
 
-	updateWindow();
+	while (1)
+	{
+		userClick();
+
+		updateWindow();
+	}
 
 	system("pause");
 	return 0;

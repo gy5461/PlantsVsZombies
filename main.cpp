7087dec4b9d1c5cb543c1600a173cabea0577d1c
gy5461 <1900269631@qq.com>
2023-10-08 23:07:31 +0800

    随机生成阳光

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <graphics.h>	//easyx图形库的头文件，需要安装easyx图形库
+#include <time.h>
 #include "tools.h"
 
 #define WIN_WIDTH 900
@@ -37,6 +38,18 @@ struct Plant
 };
 
 struct Plant map[3][9];
+
+struct SunshineBall
+{
+	int x, y;		// 阳光球在飘落过程中的坐标位置（x不变）
+	int frameIndex;	// 当前显示的图片帧序号
+	int destY;		// 飘落的目标位置的y坐标
+	bool used;		// 是否在使用
+	int timer;
+};
+
+struct SunshineBall balls[10];
+IMAGE imgSunshineBall[29];
 
 bool fileExist(const char* name)
 {
@@ -85,6 +98,17 @@ void gameInit()
 	}
 
 	curPlant = 0;
+
+	memset(balls, 0, sizeof(balls));
+	for (int i = 0; i < 29; i++)
+	{
+		sprintf_s(name, sizeof(name), "res/sunshine/%d.png", i + 1);
+		loadimage(&imgSunshineBall[i], name);
+	}
+
+	// 配置随机种子
+	srand(time(NULL));
+
 	// 创建游戏的图形窗口
 	initgraph(WIN_WIDTH, WIN_HEIGHT, 1);
 }
@@ -123,6 +147,16 @@ void updateWindow()
 	{
 		IMAGE* img = imgPlants[curPlant - 1][0];
 		putimagePNG(curX - img->getwidth() / 2, curY - img->getheight() / 2, img);
+	}
+
+	int ballMax = sizeof(balls) / sizeof(balls[0]);
+	for (int i = 0; i < ballMax; i++)
+	{
+		if (balls[i].used)
+		{
+			IMAGE* img = &imgSunshineBall[balls[i].frameIndex];
+			putimagePNG(balls[i].x, balls[i].y, img);
+		}
 	}
 
 	EndBatchDraw();	//结束双缓冲
@@ -171,6 +205,60 @@ void userClick()
 
 }
 
+void createSunshine()
+{
+	static int count = 0;
+	static int fre = 400;
+	count++;
+	if (count >= fre)
+	{
+		fre = 200 + rand() % 200;
+		count = 0;
+
+		// 从阳光池中取一个可以使用的
+		int ballMax = sizeof(balls) / sizeof(balls[0]);
+
+		int i;
+		for (i = 0; i < ballMax && balls[i].used; i++);
+		if (i >= ballMax)
+		{
+			return;
+		}
+
+		balls[i].used = true;
+		balls[i].frameIndex = 0;
+		balls[i].x = 260 + rand() % (900 - 260);	// 260..900
+		balls[i].y = 60;
+		balls[i].destY = 200 + (rand() % 4) * 90;	// 0..3
+		balls[i].timer = 0;
+	}
+}
+
+void updateSunshine()
+{
+	int ballMax = sizeof(balls) / sizeof(balls[0]);
+	for (int i = 0; i < ballMax; i++)
+	{
+		if (balls[i].used)
+		{
+			balls[i].frameIndex = (balls[i].frameIndex + 1) % 29;
+			if (balls[i].timer == 0)
+			{
+				balls[i].y += 2;
+			}
+
+			if (balls[i].y >= balls[i].destY)
+			{
+				balls[i].timer++;
+				if (balls[i].timer > 100)
+				{
+					balls[i].used = false;
+				}
+			}
+		}
+	}
+}
+
 void updateGame()
 {
 	for (int i = 0; i < 3; i++)
@@ -189,19 +277,49 @@ void updateGame()
 			}
 		}
 	}
+
+	createSunshine();	// 创建阳光
+	updateSunshine();	// 更新阳光状态
 }
 
 void startUI()
 {
 	IMAGE imgBg, imgMenu1, imgMenu2;
 	loadimage(&imgBg, "res/menu.png");
-	loadimage(&imgBg, "res/menu1.png");
-	loadimage(&imgBg, "res/menu2.png");
+	loadimage(&imgMenu1, "res/menu1.png");
+	loadimage(&imgMenu2, "res/menu2.png");
+	int flag = 0;
+
+	while (1)
+	{
+		BeginBatchDraw();
+		putimage(0, 0, &imgBg);
+		putimagePNG(474, 75, flag ? &imgMenu2 : &imgMenu1);
+
+		ExMessage msg;
+		if (peekmessage(&msg))
+		{
+			if (msg.message == WM_LBUTTONDOWN &&
+				msg.x > 474 && msg.x < 474 + 300 &&
+				msg.y > 75  && msg.y < 75 + 140)
+			{
+				flag = 1;
+			}
+			else if (msg.message == WM_LBUTTONUP && flag)
+			{
+				return;
+			}
+		}
+
+		EndBatchDraw();
+	}
 }
 
 int main(void)
 {
 	gameInit();
+
+	startUI();
 
 	int timer = 0;
 	bool flag = true;
